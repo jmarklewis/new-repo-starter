ba1bf610828c0b57e42a07ae42ce6e77ddd8c9d5
jmarklewis <jmarklewis21@gmail.com>
2023-02-22 10:07:49 -0500

    I added my answers

--- tell-us-about-yourself.js
+++ tell-us-about-yourself.js
@@ -1,11 +1,12 @@
 // Tell us about yourself 
 
 // 1. Where do you call home? 
-
+Milford, CT
 // 2. Have you lived anywhere else? If so where? 
-
+New York, NY and Jersey City , NJ
 // 3. What was your most recent job? What did you enjoy about that role?
-
+I worked as service writer at an independent Porsche shop
 // 4. What is your "comfort" food? 
-
+spiced peaches
 // 5. What is one thing you find interesting about yourself ( that others might not notice at first) ? 
+I went on a date with Linus Torvald's daughter a while ago
